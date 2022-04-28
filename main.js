1d65496fd26393792f3a0529e4b1906d633b6bac
HarryPotter1234567890 <100985749+HarryPotter1234567890@users.noreply.github.com>
2022-04-28 18:50:16 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("-----LESSON 1-----");
+console.log("See that thing on the top? IGNORE THAT. THAT LINK LEADS TO A VIRUS THAT CAN HARM YOUR COMPUTER FOREVER");
+console.log("Make sure to have paper and pen because we are not coding yet. To print, go to js and type: console.log('string means put anything here or write')");
+console.log("Now normally, we would be focussing on one topic, but you can use printing in important and unique ways.");
+console.log("This is an example: console.log(variable)");
+console.log("There is no string because it's a variable. Variables can store numbers, strings and more. Like this: x = 0;");
+console.log("That x is your variable name. When you code further, you need to use is at some point so that you can edit your variable.");
+console.log("I also forgot: ALWAYS PUT SEMI-COLUMNS AT THE END OF ALMOST EVERY LINE!!!!!!");
+console.log(109*102012);
+console.log("That number on the top was solved by the computer.");
+console.log("Write: console.log(932*345);");
+console.log("* means multiply, / means divide, + means add, - means subtract.")
+console.log("This is going to be the end of it. Bye!!!");
